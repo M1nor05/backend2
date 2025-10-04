634fecf3c83d8edc20d33f1755aefbe4b06a2d95
M1nor05 <yashnarzaripov068@gmail.com>
2025-10-04 14:54:58 +0300

    mshdjbd

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -1,4 +1,4 @@
-import { registerAs } from '@nestjs/config';
+import { registerAs } from "@nestjs/config";
 
 export default registerAs('app', () => ({
   port: Number(process.env.PORT) || 3000,
--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,24 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
-import { setupSwagger } from './config/swagger.config';
-import * as bcrypt from 'bcrypt';
-
-
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { setupSwagger } from "./config/swagger.config";
+import { ConfigService } from "@nestjs/config";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-
-    setupSwagger(app);
+  setupSwagger(app);
 
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT') || 3000;
+  const port = configService.get<number>('app.port') || 3000; // ❌ o‘rniga 'app.port'
 
-  
   app.setGlobalPrefix('api');
-
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
+  app.enableCors({ origin: '*' });
 
-  
-  app.enableCors({
-    origin: '*', 
-  });
-
-
-  
-  
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0'); // Railway uchun 0.0.0.0 kerak
   console.log(`🚀 Server is running on http://localhost:${port}/api`);
-  
-const password = 'Zaripov05';
- const hash = await bcrypt.hash(password, 10);
-console.log(hash);
+
 }
 bootstrap();
